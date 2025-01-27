600bfc639322558f292d74e9ddd5f234fad4a030
Angesh Chettiar <angeshpc91@gmail.com>
2025-01-27 11:52:21 +0000

    Layout changes done with grid, scss added, image hover on zoom done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css'
+import './App.scss'
 import Card from './components/card/card'
 import forestImg from './assets/forest.png'
 import skyImg from './assets/sky.png'
 
 function App() {
   const mockCardData = [
+    {
+      subTitle: 'Volunteered in',
+      mainTitle: '',
+      bg: '#000',
+    },
     {
       subTitle: 'Volunteered Time',
       mainTitle: '56 Hours',
@@ -18,6 +23,7 @@ function App() {
     {
       mainTitle: 'Jobs Available',
       subTitle: 'Number of impressions',
+      bg: '#fff',
       data: [
         {
           place: 'Tata Memorial Hospital',
@@ -46,22 +52,25 @@ function App() {
         },   
       ]
     },
+    
     {
       subTitle: 'Next Coverage increase',
       mainTitle: '₹1,00,000',
       bottomTitle:'Increase coverage by volunteering for 4 hours',
       bg: skyImg,
     },
+    
   ]
   return (
-    <div className="container">
-      <h1>Dashboard Design</h1>
+    <main className="container">
+      {/* <h1>Dashboard Design</h1> */}
+      <nav></nav>
       <div className='card-container'>
         {mockCardData.map((card, index) => (
           <Card key={index} {...card} />
         ))}
       </div>
-    </div>
+    </main>
   )
 }
 
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import './card.css';
+import './card.scss';
 import { ICardProps } from './card.types';
 
 const Card: React.FC<ICardProps> = (props) => {
     const { mainTitle, subTitle, bg } = props;
     return (
-        <div className="card" style={{ background: bg?.charAt(0) !== '#'? `url(${bg}) no-repeat fixed center` : bg }}>
-            <p>{subTitle}</p>
-            <h2>{mainTitle}</h2>
+        <div className="card" style={{background: bg?.charAt(0) === '#'? bg : 'none'}}>
+            {bg?.charAt(0) !== '#' &&
+                <img src={bg} alt={mainTitle}/>
+            }
+            <div className="card-content">
+                <p>{subTitle}</p>
+                <h2>{mainTitle}</h2>
+            </div>
         </div>
     );
 };
